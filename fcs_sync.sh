176ad48d7950c6a9646d8a1300e815c5979fe3fb
Olimpiu Rob <olimpiu.rob@gmail.com>
2017-10-05 13:22:57 +0300

    Refs #88394; Fixed single quoted variable expansion

--- fcs_sync.sh
+++ fcs_sync.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl --header 'Authorization: $FGAS_TOKEN' $FGAS_SERVER_URL/sync/fgases; curl --header 'Authorization: $FGAS_TOKEN' $FGAS_SERVER_URL/sync/ods
+curl -v --header 'Authorization: '"$FGAS_TOKEN"'' $FGAS_SERVER_URL/sync/fgases; curl -v --header 'Authorization: '"$FGAS_TOKEN"'' $FGAS_SERVER_URL/sync/ods
