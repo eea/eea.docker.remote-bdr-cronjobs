900fc7bfa4ffe16290eaabe355fc4034a555a200
Olimpiu Rob <olimpiu.rob@gmail.com>
2017-10-05 14:34:58 +0300

    Refs #88394; Removed verbosity flag from curl calls

--- fcs_sync.sh
+++ fcs_sync.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -v --header 'Authorization: '"$FGAS_TOKEN"'' $FGAS_SERVER_URL/sync/fgases; curl -v --header 'Authorization: '"$FGAS_TOKEN"'' $FGAS_SERVER_URL/sync/ods
+curl --header 'Authorization: '"$FGAS_TOKEN"'' $FGAS_SERVER_URL/sync/fgases; curl --header 'Authorization: '"$FGAS_TOKEN"'' $FGAS_SERVER_URL/sync/ods
